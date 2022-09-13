bde8101ca4b9654aa3ada3b53ac17be154155134
Pravania <preliner01@gmail.com>
2022-09-13 13:45:17 -0700

    Loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * add - adding numbers
+ * @n: number 1
+ * @y: number 2
+ * Return: always 0
+ */
+
+int add(int n, int y)
+{
+	int sum;
+
+	sum = n + y;
+	_putchar(sum);
+	return (0);
+}
