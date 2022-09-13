6eb0df5fae315a4d6c718a6d0c89784bd8003a4b
Pravania <preliner01@gmail.com>
2022-09-13 12:53:03 -0700

    Loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+/**
+ * _abs - computes absolute number
+ * @n: number being checked
+ * Return: always 0
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{	
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+	return (0);
+}
