283b7f60074434a80b35dad7b8452d321bd14281
Pravania <preliner01@gmail.com>
2022-09-13 13:55:47 -0700

    Loops

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <studio.h>
+
+/**
+ * print_to_98 - prints natura numbers to 98
+ * @n: number
+ * Return: always 0
+ */
+
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for (n = n; n <= 97; n++)
+			printf("%d, ", n);
+	}
+	else
+		for (n = n; n <= 98; n++)
+			printf("%d, ", n);
+	printf("98\n");
+}
