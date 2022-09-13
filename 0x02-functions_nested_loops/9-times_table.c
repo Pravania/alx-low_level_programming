baffc39263363a2faf89df93ea0967d13114a941
Pravania <preliner01@gmail.com>
2022-09-13 13:37:54 -0700

    Loops

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * times_table - prints the times table of an integer starting with zero
+ * Return: always 0
+ */
+
+void times_table(void)
+{
+	int x, y, p;
+
+	for (x = 0; x <= 9; x++)
+	{
+		for (y = 0; y <= 9; y++)
+		{
+			p = x * y;
+			if (y != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			if (y == 0)
+			{
+				_putchar('0');
+			}
+			else if (p >= 10)
+			{
+				_putchar((p / 10) + '0');
+				_putchar((p % 10) + '0');
+			}
+			else if ((p < 10) && y != 0))
+			{
+				_putchar(' ');
+				_putchar((p % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
