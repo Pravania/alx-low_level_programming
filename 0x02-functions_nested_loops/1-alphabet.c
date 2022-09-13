6972f7415c6b5f00c37ce511b761739cd03507a8
Pravania <preliner01@gmail.com>
2022-09-13 09:17:25 -0700

    Loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include <main.h>
+/**
+ * print_alphabet - prints alphabet
+ */
+
+void print_alphabet(void)
+{
+	char alpha;
+
+	for(alpha = a; alpha <= z; alpha++)
+		_putchar(alpha);
+
+	_puchar('\n');
+}
