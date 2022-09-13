4c9b36a25646345db33627735727735028998950
Pravania <preliner01@gmail.com>
2022-09-13 14:30:11 -0700

    Loops

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,7 +7,7 @@
 
 void print_times_table(int n)
 {
-	int n, m, p;
+	int num, m, p;
 
 	if (n >= 0 && n <= 15)
 	{
@@ -15,12 +15,12 @@ void print_times_table(int n)
 		{
 		_putchar('0');
 
-			for (mult = 1; mult <= n; mult++)
+			for (m = 1; m <= n; m++)
 			{
 				_putchar(',');
 				_putchar(' ');
 
-				p = n * m;
+				p = num * m;
 
 				if (p <= 99)
 					_putchar(' ');
@@ -30,13 +30,13 @@ void print_times_table(int n)
 				if (p >= 100)
 				{
 					_putchar((p / 100) + '0');
-					_putchar(((prod / 10)) % 10 + '0');
+					_putchar(((p / 10)) % 10 + '0');
 				}
 				else if (p <= 99 && p >= 10)
 				{
-					_putchar((prod / 10) + '0');
+					_putchar((p / 10) + '0');
 				}
-				_putchar((prod % 10) + '0');
+				_putchar((p % 10) + '0');
 			}
 			_putchar('\n');
 		}
