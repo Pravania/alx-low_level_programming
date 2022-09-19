f0568febd8e776647d994e5dbf9eef7e88c45880
Pravania <preliner01@gmail.com>
2022-09-19 12:45:32 -0700

    pointers

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -3,12 +3,26 @@
  * _atoi - function
  * @s: string
  * Return: always 0
- *
- * */
+ */
 
 int _atoi(char *s)
+
 {
+	int sign = 1;
+	unsigned int num = 0;
 
+	do {
+		if (*s == '-')
+			sign *= -1;
 
-		return (0);
+		else if (*s >= '0' && *s <= '9')
+			num = (num * 10) + (*s - '0');
+
+		else if (num > 0)
+			break;
+
+	} while (*s++);
+
+	return (num * sign);
+
 }
