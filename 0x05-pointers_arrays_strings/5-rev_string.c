fd3494e2599c9f41714e4c9b52ecb3ac6cc0bf91
Pravania <preliner01@gmail.com>
2022-09-19 12:03:33 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *  * rev_string - this characters in reverse.
+ *   * @s: this is a parameter to change.
+ *    */
+void rev_string(char *s)
+{
+	int ini = 0, f = 0;
+	char t;
+
+	while (s[f] != 0)
+		f++;
+	f--;
+
+	while (ini < f)
+	{
+		t = s[ini];
+		s[ini] = s[f];
+		s[f] = t;
+		ini++;
+		f--;
+	}
+}
