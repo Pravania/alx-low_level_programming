22c6f1e969fca5d018cdeb796f17c526a6ae1af1
Pravania <preliner01@gmail.com>
2022-09-21 07:23:34 -0700

    more pointers

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/*
+ * _strncp - copies string
+ * @dest: buffer
+ * @src: string
+ * @n: number of bytes
+ * Return: pointer
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int index = 0, src_len = 0;
+
+	while (src[index++])
+		src_len++;
+
+	for (index = 0; src[index] && index < n; index++)
+		dest[index] = src[index];
+
+	for (index = src_len; index < n; index++)
+		dest[index] = '\0';
+
+	return (dest);
+}
