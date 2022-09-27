d1d4260a583dcc54da23b701f03937d421d369f6
Pravania <preliner01@gmail.com>
2022-09-27 12:44:15 -0700

    even more pointers

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * memset - fills memory
+ * @s: pointer to memory to fill
+ * @c: character to fille memory with
+ * @n: number of bytes
+ * Return: pointer to filled memory @s
+ */
+void *_memset(void *s, int c, size_t n)
+{
+	unsigned int index;
+	unsigned char *memory = s, value = c;
+
+	for (index = 0; index < n; index++)
+		memory[index] = value;
+
+	return (memory);
+}
